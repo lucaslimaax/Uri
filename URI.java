81112cde34ebc5c2996dc36e0f30ece0b5f1bbff
Lucas Lima <38673203+lucaslimaax@users.noreply.github.com>
2019-01-28 20:16:06 -0200

    Uri - 1001
    beginner

---
+++ URI.java
@@ -0,0 +1,26 @@
+
+package uri;
+
+import java.util.Scanner;
+
+
+
+public class URI {
+
+   
+    public static void main(String[] args) {
+       int A,B, X;
+       Scanner entrada  = new Scanner(System.in);
+        System.out.println("Digite um valor para A: ");
+        A= entrada.nextInt();
+        System.out.println("Digite um valor para B: ");
+        B= entrada.nextInt();
+        
+       X=A+B;
+       
+        System.out.println(" X = "+X+"\n");
+        
+       
+    }
+    
+}
